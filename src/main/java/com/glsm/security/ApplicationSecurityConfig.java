70e95ba202a0a23e89f62362986a8474f207008f
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2023-06-03 16:43:00 +0300

    Form_Based_Authentication

--- src/main/java/com/glsm/security/ApplicationSecurityConfig.java
+++ src/main/java/com/glsm/security/ApplicationSecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.glsm.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.glsm.security.ApplicationUserRole.*;
@@ -61,7 +64,29 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()//tüm URL'lerin yetkilendirme kuralına tabi olacağını ifade eder.
                 .authenticated()//yetkilendirilmemiş isteklerin reddedileceğini ve sadece kimlik doğrulama geçmişi olan kullanıcılara izin verileceğini belirtir.
                 .and()//
-                .httpBasic(); //HTTP temel kimlik doğrulama yöntemini kullanarak kimlik doğrulama yapılacağını belirtir
+              //  .httpBasic(); //HTTP temel kimlik doğrulama yöntemini kullanarak kimlik doğrulama yapılacağını belirtir
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll() // switch basic Auth to from based Auth
+                    .defaultSuccessUrl("/courses",true)
+                    .passwordParameter("password")
+                    .usernameParameter("username")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured")
+                .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher( new AntPathRequestMatcher("/logout","GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login");
+
+
+
 
     }
 
